2e368a7e9750364f8f444ac8251a1d96a5193261
Prince Sharma <princesharma21102004@gmail.com>
2025-05-29 21:20:01 +0530

    small change

--- bgChanger/src/components/nevBar.jsx
+++ bgChanger/src/components/nevBar.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 export default function NevBar(props) {
   return (
     <nav className="fixed bottom-4 left-1/2 -translate-x-1/2 z-[1000]">
-      <div className="inline-flex bg-gray-900 rounded-full px-4 py-2 shadow-lg" style={{backgroundColor:"offwhite", backdropFilter: "blur(10px)" }}>
+      <div className="inline-flex bg-gray-900 rounded-full px-4 py-2 shadow-lg" style={{backgroundColor:"white", backdropFilter: "blur(10px)" }}>
 
         <Button color="gold" setColor={props.setColor} className="mx-2" />
         <Button color="crimson" setColor={props.setColor} className="mx-2" />
